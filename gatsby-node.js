7ce0fa94efa18f75feb71a1c0a121a0c8ecce76d
Mattie <matthew.ellins@gmail.com>
2022-03-24 22:36:53 +0800

    Build to docs instead of public

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,25 @@
 const path = require(`path`)
 const _ = require('lodash');
 const { createFilePath } = require(`gatsby-source-filesystem`)
+const fs = require("fs")
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
